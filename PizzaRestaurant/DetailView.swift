69d6081c93619903cd1f4e3dbd3ee9dee9a6087d
Alexandre Lordelo <alexandre.lordelo@icloud.comn>
2020-11-30 13:07:51 +0100

    Initial Commit

---
+++ PizzaRestaurant/DetailView.swift
@@ -0,0 +1,51 @@
+//
+//  DetailView.swift
+//  PizzaRestaurant
+//
+//  Created by Alexandre Lordelo on 2020-11-30.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    var order = Order()
+    
+    @State var showOrderEdit = false
+
+
+    var body: some View {
+        
+        Form{
+        
+        Text(order.tableNumber)
+        Text(order.pizzaType)
+            
+        }
+        .navigationTitle(order.pizzaType)
+        
+        .toolbar {
+
+            ToolbarItem(placement: .primaryAction) {
+                
+                
+                //edit button
+                Button(action: {
+                    showOrderEdit = true
+                }, label: {
+                    Text("Edit")
+                })
+                .sheet(isPresented: $showOrderEdit) {
+                    OrderEdit(order: order)
+                }
+            }
+        }
+
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
